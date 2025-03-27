3edb08b933ab20b449bdd90931eec912dfcadccd
Krister Lawlor <KristerJLawlor@gmail.com>
2025-03-26 20:18:31 -0700

    Can now send data to a MongoDB Cluster via Post()

--- src/index.ts
+++ src/index.ts
@@ -1,38 +1,59 @@
-// index.ts
+//index.ts
 
-// Import express library
+//imports
 import express, { Request, Response } from 'express';
+import mongoose from 'mongoose';
+import cors from 'cors';  //mechanism to safely bypass the same-origin policy, that is, it allows a web page to access restricted resources from a server on a domain different than the domain that served the web page
+import mongoData from './mongoData';
 
-// Creates an express object
+//app config
 const app = express();
+const port = 3000;
 
-app.use(express.json());
+//middleware
+app.use(express.json());  //define what express will parse
+app.use(cors());  //cross origin resource sharing. Allows access from other domains (origins)
 
-// It listens to HTTP get request. 
-// Here it listens to the root i.e '/'
+//db config   pass: KBw3wxv9DMVVKtwH
+const mongoURI = 'mongodb+srv://admin:KBw3wxv9DMVVKtwH@cluster0.b17mu.mongodb.net/DiscloneDB?retryWrites=true&w=majority&appName=Cluster0';
+
+mongoose.connect(mongoURI);
+
 app.get("/", (req: Request, res: Response) => {
 
-  // Using send function we send
-  // response to the client
-  // Here we are sending html
   res.send("<h1> Hello World </h1>");
+
 });
 
+
+//api routes
 app.post("/api/sendMessage", (req: Request, res: Response) => {
 
-  console.log(req.body);//req.body.user + " has logged in to " + req.body.channel + " channel.");  //JSON data logged
+  console.log(req.body);  //JSON data logged
 
   res.send("<h2> You have triggered post! </h2>");     //echo result back
+
 });
 
-// It configures the system to listen
-// to port 3000. Any number can be 
-// given instead of 3000, the only
-// condition is that no other server
-// should be running at that port
-app.listen(3000, () => {
+app.post('/new/channel', (req: Request, res: Response) => {
 
-  // Print in the console when the
-  // servers starts to listen on 3000
+  console.log(req.body);
+
+  const dbData = req.body;  //save data to immutable variable
+
+  //create file from request
+  mongoData.create(dbData)
+    .then((result) => {res.status(201).send(dbData); }) //respond with success code
+
+    .catch((err) => {res.status(500).send(err)});  //respond with server error code
+
+    
+  
+});
+
+
+app.listen(port, () => {
+
   console.log("Listening to port 3000");
+
 });
